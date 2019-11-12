12d00567fae87ba174083a4350eec9ec03729c3f
uxrgrtjigtiyrt <rovin2393@naver.com>
2019-11-12 18:53:59 +0900

    a

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <div className="div1">hello world</div>
+    <div className="div2">hello world</div>
+    <div className="div3">hello world</div>
+    <div className="div4">hello world</div>
+    <div className="div5">hello world</div>
+    <div className="div6">hello world</div>
+  </div>
+  
+    );
+  }
+
+value=>(value+2)
+
+export default App;
